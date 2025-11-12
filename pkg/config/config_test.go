51e1eb8048a8c3a4d6b017413cf5635aea8b60b2
Paulo Dias <paulodias.gm@gmail.com>
2025-11-12 14:45:30 +0000

    chore: move to require
    Signed-off-by: Paulo Dias <paulodias.gm@gmail.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -3,7 +3,6 @@ package config
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -23,19 +22,19 @@ func TestLoadConfig(t *testing.T) {
 				require.Len(t, cfg.ServerGroups, 2)
 
 				// Verify first server group
-				assert.Equal(t, "loki1", cfg.ServerGroups[0].Name)
-				assert.Equal(t, "http://loki1.example.com", cfg.ServerGroups[0].URL)
-				assert.Equal(t, 10, cfg.ServerGroups[0].Timeout)
-				assert.Equal(t, "Bearer token1", cfg.ServerGroups[0].Headers["Authorization"])
+				require.Equal(t, "loki1", cfg.ServerGroups[0].Name)
+				require.Equal(t, "http://loki1.example.com", cfg.ServerGroups[0].URL)
+				require.Equal(t, 10, cfg.ServerGroups[0].Timeout)
+				require.Equal(t, "Bearer token1", cfg.ServerGroups[0].Headers["Authorization"])
 
 				// Verify second server group
-				assert.Equal(t, "loki2", cfg.ServerGroups[1].Name)
-				assert.Equal(t, "http://loki2.example.com", cfg.ServerGroups[1].URL)
-				assert.Equal(t, 15, cfg.ServerGroups[1].Timeout)
+				require.Equal(t, "loki2", cfg.ServerGroups[1].Name)
+				require.Equal(t, "http://loki2.example.com", cfg.ServerGroups[1].URL)
+				require.Equal(t, 15, cfg.ServerGroups[1].Timeout)
 
 				// Verify logging config
-				assert.Equal(t, "info", cfg.Logging.Level)
-				assert.Equal(t, "json", cfg.Logging.Format)
+				require.Equal(t, "info", cfg.Logging.Level)
+				require.Equal(t, "json", cfg.Logging.Format)
 			},
 		},
 		{
@@ -46,9 +45,9 @@ func TestLoadConfig(t *testing.T) {
 				require.NotNil(t, cfg)
 				require.Len(t, cfg.ServerGroups, 1)
 
-				assert.Equal(t, "loki1", cfg.ServerGroups[0].Name)
-				assert.Equal(t, "http://localhost:3100", cfg.ServerGroups[0].URL)
-				assert.Equal(t, 0, cfg.ServerGroups[0].Timeout) // Default not set in config
+				require.Equal(t, "loki1", cfg.ServerGroups[0].Name)
+				require.Equal(t, "http://localhost:3100", cfg.ServerGroups[0].URL)
+				require.Equal(t, 0, cfg.ServerGroups[0].Timeout) // Default not set in config
 			},
 		},
 		{
@@ -60,21 +59,21 @@ func TestLoadConfig(t *testing.T) {
 				require.Len(t, cfg.ServerGroups, 2)
 
 				// Verify first server group with full TLS config
-				assert.Equal(t, "loki-production", cfg.ServerGroups[0].Name)
-				assert.Equal(t, "https://loki.example.com", cfg.ServerGroups[0].URL)
-				assert.Equal(t, 30, cfg.ServerGroups[0].Timeout)
-				assert.Equal(t, "Bearer prod-token", cfg.ServerGroups[0].Headers["Authorization"])
-				assert.Equal(t, "tenant-1", cfg.ServerGroups[0].Headers["X-Scope-OrgID"])
+				require.Equal(t, "loki-production", cfg.ServerGroups[0].Name)
+				require.Equal(t, "https://loki.example.com", cfg.ServerGroups[0].URL)
+				require.Equal(t, 30, cfg.ServerGroups[0].Timeout)
+				require.Equal(t, "Bearer prod-token", cfg.ServerGroups[0].Headers["Authorization"])
+				require.Equal(t, "tenant-1", cfg.ServerGroups[0].Headers["X-Scope-OrgID"])
 
 				// Verify TLS config
-				assert.False(t, cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.InsecureSkipVerify)
-				assert.Equal(t, "/etc/ssl/certs/ca.crt", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.CAFile)
-				assert.Equal(t, "/etc/ssl/certs/client.crt", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.CertFile)
-				assert.Equal(t, "/etc/ssl/private/client.key", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.KeyFile)
+				require.False(t, cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.InsecureSkipVerify)
+				require.Equal(t, "/etc/ssl/certs/ca.crt", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.CAFile)
+				require.Equal(t, "/etc/ssl/certs/client.crt", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.CertFile)
+				require.Equal(t, "/etc/ssl/private/client.key", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.KeyFile)
 
 				// Verify logging config
-				assert.Equal(t, "debug", cfg.Logging.Level)
-				assert.Equal(t, "logfmt", cfg.Logging.Format)
+				require.Equal(t, "debug", cfg.Logging.Level)
+				require.Equal(t, "logfmt", cfg.Logging.Format)
 			},
 		},
 		{
@@ -85,11 +84,11 @@ func TestLoadConfig(t *testing.T) {
 				require.NotNil(t, cfg)
 				require.Len(t, cfg.ServerGroups, 1)
 
-				assert.Equal(t, "secure-loki", cfg.ServerGroups[0].Name)
-				assert.Equal(t, "https://loki.secure.example.com", cfg.ServerGroups[0].URL)
-				assert.Equal(t, "/path/to/ca.crt", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.CAFile)
+				require.Equal(t, "secure-loki", cfg.ServerGroups[0].Name)
+				require.Equal(t, "https://loki.secure.example.com", cfg.ServerGroups[0].URL)
+				require.Equal(t, "/path/to/ca.crt", cfg.ServerGroups[0].HTTPClientConfig.TLSConfig.CAFile)
 
-				assert.Equal(t, "warn", cfg.Logging.Level)
+				require.Equal(t, "warn", cfg.Logging.Level)
 			},
 		},
 		{
